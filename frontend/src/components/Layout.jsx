3efec70099b8f53e2f794648c65272950a4fe7f6
ChesterLen <svetoslavbaykov55@gmail.com>
2026-06-05 18:46:55 +0300

    Implement dynamic page title update

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,8 +2,20 @@ import React from "react"
 import Header from "./Header"
 import Main from "./Main"
 import Footer from "./Footer"
+import { useLocation } from "react-router-dom"
 
 export default function Layout() {
+    const location = useLocation().pathname
+    const title = document.getElementsByTagName("title")[0]
+
+    React.useEffect(() => {
+        if (location === "/") {
+            title.textContent = "Home"
+        } else {
+            title.textContent = `${location.slice(1, 2).toUpperCase()}${location.slice(2, location.length)}`
+        }
+    }, [location])
+    
     return (
         <>
             <div className="h-m-container">
