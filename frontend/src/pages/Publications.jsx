832823b9082699a6ebd165726ed4fcf03dc63cdb
ChesterLen <svetoslavbaykov55@gmail.com>
2026-06-05 23:37:10 +0300

    Update CSS code

--- frontend/src/pages/Publications.jsx
+++ frontend/src/pages/Publications.jsx
@@ -1,14 +1,11 @@
 import React from "react"
 
 export default function Publications() {
-    const headerMainContainer = document.getElementsByClassName("h-m-container")[0]
-    // headerMainContainer.styles.backgroundImage = "../assets/ChatGPT\ Image\ Jun\ 4\,\ 2026\,\ 04_30_19\ PM.png"
     return (
         <div className="publications-container">
             <h1>Publications</h1>
             <div className="outer-container">
                 <div className="inner-container">
-                    <p>Inner container</p>
                 </div>
             </div>
         </div>
